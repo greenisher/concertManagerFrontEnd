e91d1f9c02747c67f0b78f24f098f23ebdef421a
bgreen2 <bgreen@and.digital>
2019-04-05 15:12:26 +0100

    first commit

---
+++ src/GroupEdit.js
@@ -0,0 +1,113 @@
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import { Button, Container, Form, FormGroup, Input, Label } from "reactstrap";
+import AppNavbar from "./AppNavbar";
+
+class GroupEdit extends Component {
+  emptyItem = {
+    name: "",
+    genre: "",
+    concerts: ""
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: this.emptyItem
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async componentDidMount() {
+    if (this.props.match.params.id !== "new") {
+      const group = await (await fetch(
+        `/api/group/${this.props.match.params.id}`
+      )).json();
+      this.setState({ item: group });
+    }
+  }
+
+  handleChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+    let item = { ...this.state.item };
+    item[name] = value;
+    this.setState({ item });
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    const { item } = this.state;
+
+    await fetch("/api/group", {
+      method: item.id ? "PUT" : "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(item)
+    });
+    this.props.history.push("/groups");
+  }
+
+  render() {
+    const { item } = this.state;
+    const title = <h2>{item.id ? "Edit Group" : "Add Group"}</h2>;
+
+    return (
+      <div>
+        <AppNavbar />
+        <Container>
+          {title}
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="name">Name</Label>
+              <Input
+                type="text"
+                name="name"
+                id="name"
+                value={item.name || ""}
+                onChange={this.handleChange}
+                autoComplete="name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="genre">Genre</Label>
+              <Input
+                type="text"
+                name="genre"
+                id="genre"
+                value={item.genre || ""}
+                onChange={this.handleChange}
+                autoComplete="genre"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="concerts">Concerts</Label>
+              <Input
+                type="text"
+                name="concerts"
+                id="concers"
+                value={item.concerts || ""}
+                onChange={this.handleChange}
+                autoComplete="concerts"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Button color="primary" type="submit">
+                Save
+              </Button>{" "}
+              <Button color="secondary" tag={Link} to="/groups">
+                Cancel
+              </Button>
+            </FormGroup>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default withRouter(GroupEdit);
---
+++ src/GroupList.js
@@ -0,0 +1,110 @@
+import React, { Component } from "react";
+import { Button, ButtonGroup, Container, Table } from "reactstrap";
+import AppNavbar from "./AppNavbar";
+import { Link } from "react-router-dom";
+
+class GroupList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { groups: [], isLoading: true };
+    this.remove = this.remove.bind(this);
+  }
+
+  componentDidMount() {
+    this.setState({ isLoading: true });
+
+    fetch("api/groups")
+      .then(response => response.json())
+      .then(data => this.setState({ groups: data, isLoading: false }));
+  }
+
+  async remove(id) {
+    await fetch(`api/group/${id}`, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    }).then(() => {
+      let updatedGroups = [...this.state.groups].filter(i => i.id !== id);
+      this.setState({ groups: updatedGroups });
+    });
+  }
+
+  render() {
+    const { groups, isLoading } = this.state;
+
+    if (isLoading) {
+      return <p>Loading. . .</p>;
+    }
+
+    const groupList = groups.map(group => {
+      return (
+        <tr key={group.id}>
+          <td style={{ whiteSpace: "nowrap" }}>{group.name}</td>
+          <td style={{ whiteSpace: "nowrap" }}>{group.genre}</td>
+          <td>
+            {group.concerts.map(concert => {
+              return (
+                <div key={concert.id}>
+                  {new Intl.DateTimeFormat("en-GB", {
+                    year: "2-digit",
+                    month: "2-digit",
+                    day: "2-digit"
+                  }).format(new Date(concert.date))}
+                  : {concert.location}
+                </div>
+              );
+            })}
+          </td>
+          <td>
+            <ButtonGroup>
+              <Button
+                size="sm"
+                color="primary"
+                tag={Link}
+                to={"/group/" + group.id}
+              >
+                Edit
+              </Button>
+              <Button
+                size="sm"
+                color="danger"
+                onClick={() => this.remove(group.id)}
+              >
+                Delete
+              </Button>
+            </ButtonGroup>
+          </td>
+        </tr>
+      );
+    });
+
+    return (
+      <div>
+        <AppNavbar />
+        <Container fluid>
+          <div className="float-right">
+            <Button color="success" tag={Link} to="/group/new">
+              Add Group
+            </Button>
+          </div>
+          <h3>Upcoming Concerts</h3>
+          <Table className="mt-4">
+            <thead>
+              <tr>
+                <th width="25%">Name</th>
+                <th width="25">Genre</th>
+                <th width="25%">Next Playing</th>
+                <th width="25%">Edit</th>
+              </tr>
+            </thead>
+            <tbody>{groupList}</tbody>
+          </Table>
+        </Container>
+      </div>
+    );
+  }
+}
+
+export default GroupList;
